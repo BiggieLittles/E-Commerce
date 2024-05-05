db7dc307590a1793798f30afb66ac57d21aa9a4b
BiggieLittles <156357409+BiggieLittles@users.noreply.github.com>
2024-05-04 20:02:12 -0400

    somewhat functional

---
+++ seeds/category-seeds.js
@@ -0,0 +1,23 @@
+const { Category } = require('../models');
+
+const categoryData = [
+  {
+    category_name: 'Shirts',
+  },
+  {
+    category_name: 'Shorts',
+  },
+  {
+    category_name: 'Music',
+  },
+  {
+    category_name: 'Hats',
+  },
+  {
+    category_name: 'Shoes',
+  },
+];
+
+const seedCategories = () => Category.bulkCreate(categoryData);
+//it defines the object that is created when a file is imported using require seedCategories
+module.exports = seedCategories;
